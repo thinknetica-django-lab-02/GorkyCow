cb960f3eb2913285a2760050b7ed79912bd75fb2
Vitaly Kuznetsov <gorkycow@gmail.com>
2021-03-26 17:26:13 +0300

    Add ckeditor, reregister FlatPageAdmin, add requirments.txt, some code formating with black and isort to meet PEP8 requirments

--- e_commerce/main/admin.py
+++ e_commerce/main/admin.py
@@ -1,3 +1,21 @@
+from ckeditor.widgets import CKEditorWidget
+from django import forms
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
 
-# Register your models here.
+
+class FlatPageAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = FlatPage
+        fields = "__all__"
+
+
+class FlatPageAdmin(FlatPageAdmin):
+    form = FlatPageAdminForm
+
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
