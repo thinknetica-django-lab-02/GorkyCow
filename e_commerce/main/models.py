ebe167c127596947c150af4ff98a26e373e077e1
Vitaly Kuznetsov <gorkycow@gmail.com>
2021-03-27 18:27:59 +0300

    Add models for goods, categories, sellers and tags. Add migrations

--- e_commerce/main/models.py
+++ e_commerce/main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Seller(models.Model):
+    STATUSES = (
+        ('A', 'Active'),
+        ('P', 'Partner'),
+        ('T', 'Test'),
+        ('D', 'Disabled'),
+    )
+    name = models.CharField(max_length=80)
+    status = models.CharField(max_length=1, choices=STATUSES, default='A')
+    rating = models.FloatField()
+    email = models.EmailField(max_length=254)
+
+
+class Category(models.Model):
+    name =  models.CharField(max_length=80)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=80)
+
+
+class Goods(models.Model):
+    SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, verbose_name="the related seller")
+    weight = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="the related category", null=True)
+    manufacturer = models.CharField(max_length=80)
+    tags = models.ManyToManyField(Tag)
+    size = models.CharField(max_length=1, choices=SIZES, null=True)
+    rating = models.FloatField()
