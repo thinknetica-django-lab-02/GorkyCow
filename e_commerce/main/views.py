948d9199a9fb780ce366976ac1a2538be354c66c
Vitaly Kuznetsov <gorkycow@gmail.com>
2021-04-03 16:42:11 +0300

    Created a new custom tag and a custom filter

--- e_commerce/main/views.py
+++ e_commerce/main/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 
 def index(request):
     turn_on_block = True
+    text_for_filter = "Братухе подари на днюху черный орфографический словарь"
     return render(
         request,
         "main/index.html",
-        {"turn_on_block": turn_on_block, "user": request.user},
+        {
+            "turn_on_block": turn_on_block,
+            "user": request.user,
+            "text_for_filter": text_for_filter,
+        },
     )
