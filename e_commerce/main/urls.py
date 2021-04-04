6243a56f5585a013e511e02d20d76d90cb63ad60
Vitaly Kuznetsov <gorkycow@gmail.com>
2021-04-04 20:19:46 +0300

    Added detailed and list views for goods

--- e_commerce/main/urls.py
+++ e_commerce/main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("goods/", views.GoodsList.as_view()),
+    path('goods/<int:pk>/', views.GoodsDetail.as_view(), name='goods-detail'),
 ]
--- e_commerce/main/views.py
+++ e_commerce/main/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from main.models import Goods, Tag
+
+
+class GoodsList(ListView):
+    model = Goods
+    context_object_name = "goods"
+
+
+class GoodsDetail(DetailView):
+    queryset = Goods.objects.all()
+    context_object_name = 'goods'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag_list"] = Tag.objects.all()
+        return context
 
 
 def index(request):
