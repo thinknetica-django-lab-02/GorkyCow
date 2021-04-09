f4de522576edcf7537c6923892e79108407483e4
Vitaly Kuznetsov <gorkycow@gmail.com>
2021-04-09 15:06:13 +0300

    Added create and edit forms for goods

--- e_commerce/main/urls.py
+++ e_commerce/main/urls.py
@@ -8,6 +8,16 @@ urlpatterns = [
     path("goods/", views.GoodsList.as_view(), name="goods"),
     path("goods/<int:pk>/", views.GoodsDetail.as_view(), name="goods-detail"),
     path(
+        "goods/create/",
+        login_required(views.GoodsCreate.as_view()),
+        name="goods-create",
+    ),
+    path(
+        "goods/edit/<int:pk>/",
+        login_required(views.GoodsUpdate.as_view()),
+        name="goods-edit",
+    ),
+    path(
         "accounts/profile/<int:pk>/",
         login_required(views.ProfileUpdate.as_view()),
         name="profile",
