6234c084ffb7f868d9fb82bbb4f0527e088ac296
Vitaly Kuznetsov <gorkycow@gmail.com>
2021-04-18 01:43:53 +0300

    Added scheduled task for celery

--- e_commerce/e_commerce/celery.py
+++ e_commerce/e_commerce/celery.py
@@ -1,6 +1,10 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
+
+# from main.tasks import send_weekly_new_goods_email_task
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "e_commerce.settings")
 
@@ -9,3 +13,11 @@ app = Celery("e_commerce")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "send_weekly_new_goods_email_task": {
+        "task": "main.tasks.send_weekly_new_goods_email_task",
+        # "schedule": crontab(hour=1, minute=21, day_of_week="sunday"),
+        "schedule": crontab(minute="*/1"),
+    },
+}
--- e_commerce/main/tasks.py
+++ e_commerce/main/tasks.py
@@ -1,13 +1,15 @@
+from datetime import datetime, timedelta
+
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.apps import apps
 from django.contrib.auth.models import User
 
-from .messages import send_welcome_email, new_goods_subscribers_notification
-
+from .messages import (new_goods_subscribers_notification,
+                       new_goods_subscribers_weekly_notification,
+                       send_welcome_email)
 
 logger = get_task_logger(__name__)
-
 
 
 @shared_task
@@ -19,9 +21,24 @@ def send_welcome_email_task(user_id):
 
 @shared_task
 def send_new_goods_subscribers_notification_task(goods_id, profile_id):
-    logger.info("Sent new goods email")
+    logger.info("Sending new goods email")
     profile_model = apps.get_model("main.Profile")
     profile = profile_model.objects.get(id=profile_id)
     goods_model = apps.get_model("main.Goods")
     goods = goods_model.objects.get(id=goods_id)
     new_goods_subscribers_notification(goods, profile)
+
+
+@shared_task
+def send_weekly_new_goods_email_task():
+    logger.info("Sending new goods email")
+    start_date = datetime.now() - timedelta(days=7)
+    goods_model = apps.get_model("main.Goods")
+    new_goods = goods_model.objects.filter(creation_date__gte=start_date)
+    subscription_model = apps.get_model("main.Subscriptions")
+    subscription = subscription_model.objects.get(name="New goods")
+    profile_model = apps.get_model("main.Profile")
+
+    if new_goods:
+        for profile in profile_model.objects.filter(subsciber=subscription):
+            new_goods_subscribers_weekly_notification(new_goods, profile)
