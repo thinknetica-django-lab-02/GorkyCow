2f30d1fbed1641ea186fc61aa07d989a7d3804be
Vitaly Kuznetsov <gorkycow@gmail.com>
2021-04-25 14:10:56 +0300

    Added docstrings to classes, methods and functions

--- e_commerce/main/tests/test_views.py
+++ e_commerce/main/tests/test_views.py
@@ -7,16 +7,26 @@ from main.models import Category, Goods, Seller, Subscriptions
 
 
 class GoodsListTestCase(TestCase):
+    """This class serves for testing the 'GoodsList' class-based view.
+    """
     def setUp(self):
+        """This method provides a test data setup for view's test cases.
+        """
         self.client = Client()
 
     def test_responce_status(self):
+        """This method testing that a page responds with status code 200.
+        """
         response = self.client.get(reverse_lazy("goods"))
         self.assertEqual(response.status_code, 200)
 
 
 class GoodsDetailTestCase(TestCase):
+    """This class serves for testing the 'GoodsDetail' class-based view.
+    """
     def setUp(self):
+        """This method provides a test data setup for view's test cases.
+        """
         self.client = Client()
         seller = Seller.objects.create(
             name="Bobbie's Bits", rating=5, email="bobby@bobbiesbits.com"
@@ -36,6 +46,8 @@ class GoodsDetailTestCase(TestCase):
         )
 
     def test_responce_status(self):
+        """This method testing that a page responds with status code 200.
+        """
         response = self.client.get(
             reverse_lazy("goods-detail", kwargs={"pk": self.goods.id})
         )
@@ -43,7 +55,11 @@ class GoodsDetailTestCase(TestCase):
 
 
 class GoodsCreateTestCase(TestCase):
+    """This class serves for testing the 'GoodsCreate' class-based view.
+    """
     def setUp(self):
+        """This method provides a test data setup for view's test cases.
+        """
         self.client = Client()
         self.user = User.objects.create_user(
             "john",
@@ -56,13 +72,19 @@ class GoodsCreateTestCase(TestCase):
         sellers_group.user_set.add(self.user)
 
     def test_responce_status(self):
+        """This method testing that a page responds with status code 200.
+        """
         self.client.login(username="john", password="johnpassword")
         response = self.client.get(reverse_lazy("goods-create"))
         self.assertEqual(response.status_code, 200)
 
 
 class GoodsEditTestCase(TestCase):
+    """This class serves for testing the 'GoodsEdit' class-based view.
+    """
     def setUp(self):
+        """This method provides a test data setup for view's test cases.
+        """
         self.client = Client()
         self.user = User.objects.create_user(
             "john",
@@ -91,6 +113,8 @@ class GoodsEditTestCase(TestCase):
         )
 
     def test_responce_status(self):
+        """This method testing that a page responds with status code 200.
+        """
         self.client.login(username="john", password="johnpassword")
         response = self.client.get(
             reverse_lazy("goods-edit", kwargs={"pk": self.goods.id})
@@ -99,7 +123,11 @@ class GoodsEditTestCase(TestCase):
 
 
 class ProfileUpdateTestCase(TestCase):
+    """This class serves for testing the 'ProfileUpdate' class-based view.
+    """
     def setUp(self):
+        """This method provides a test data setup for view's test cases.
+        """
         self.client = Client()
         self.user = User.objects.create_user(
             "john",
@@ -108,6 +136,8 @@ class ProfileUpdateTestCase(TestCase):
         )
 
     def test_responce_status(self):
+        """This method testing that a page responds with status code 200.
+        """
         self.client.login(username="john", password="johnpassword")
         response = self.client.get(
             reverse_lazy(
@@ -119,16 +149,26 @@ class ProfileUpdateTestCase(TestCase):
 
 
 class IndexTestCase(TestCase):
+    """This class serves for testing the 'index' view.
+    """
     def setUp(self):
+        """This method provides a test data setup for view's test cases.
+        """
         self.client = Client()
 
     def test_responce_status(self):
+        """This method testing that a page responds with status code 200.
+        """
         response = self.client.get(reverse_lazy("index"))
         self.assertEqual(response.status_code, 200)
 
 
 class LoginTestCase(TestCase):
+    """This class serves for testing the allauth login view.
+    """
     def setUp(self):
+        """This method provides a test data setup for view's test cases.
+        """
         self.client = Client()
         self.user = User.objects.create_user(
             "john",
@@ -136,7 +176,10 @@ class LoginTestCase(TestCase):
             "johnpassword"
         )
 
-    def testLogin(self):
+    def test_login(self):
+        """This method testing successful login attempt with a redirection
+        to the site's index page.
+        """
         self.client.login(username="john", password="johnpassword")
         response = self.client.get(reverse_lazy("account_login"))
         self.assertEqual(response.status_code, 302)
